f8cc5761143a5abcd28754cf91c483b54e2c5747
yanghongshen <yang2819281@gmail.com>
2023-08-16 00:08:11 +0900

    include binance-public-data submodule

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
-import sqlite3
-from utils import get_api_key
-from bnws import client_stream, client_api
+import asyncio, sqlite3
+from mdt_utils import get_api_key
+from mdt_bnws import client_stream, client_api
 
-api_key, api_secret = get_api_key()
+async def clear_db():
+    # Connect to SQLite database
+    conn = sqlite3.connect('sqlite.db')
+    cursor = conn.cursor()
+    # Delete existing data from kline table
+    cursor.execute('DELETE FROM kline;')
+    conn.commit()
+    conn.close()
 
-# Connect to SQLite database
-conn = sqlite3.connect('sqlite.db')
-cursor = conn.cursor()
-# Delete existing data from kline table
-cursor.execute('DELETE FROM kline;')
-conn.commit()
-conn.close()
+async def main():
+    api_key, api_secret = get_api_key()
+    await clear_db()
+    client_stream()
 
-client_stream()
+if __name__ == "__main__":
+    asyncio.run(main())
